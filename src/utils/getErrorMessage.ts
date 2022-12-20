732216823be3ccab1d975d4f3ce76001df71c9b1
Rustambekov-Iskander <isken.kgz.2005@gmail.com>
2022-12-20 12:16:23 +0600

    fix getErrorMessage

--- src/utils/getErrorMessage.ts
+++ src/utils/getErrorMessage.ts
@@ -1,7 +1,9 @@
 
 export const getErrorMessage = (e: any) => {
     const errorData = e?.response?.data
-    const errorMessage = errorData[Object.keys(errorData)[0]]
+    const errorMessage = errorData[Object.keys(errorData)[0]][0]
+
+    console.log(errorMessage)
     
     if (errorData && errorMessage.length > 1) {
        return errorMessage
