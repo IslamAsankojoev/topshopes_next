1712cf58309fddb7f02e40c2838dd84891457fda
Rustambekov-Iskander <isken.kgz.2005@gmail.com>
2022-12-26 09:51:30 +0600

    Payment Details

--- src/pages-sections/checkout/CheckoutForm2.tsx
+++ src/pages-sections/checkout/CheckoutForm2.tsx
@@ -1,32 +1,28 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { DeleteOutline, ModeEditOutline } from '@mui/icons-material'
+import { DeleteOutline } from '@mui/icons-material'
 import {
 	Avatar,
-	Box,
 	Button,
 	Card,
-	Checkbox,
+	FormControl,
 	Grid,
-	MenuItem,
+	Radio,
+	RadioGroup,
 	Typography,
 } from '@mui/material'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import IconButton from '@mui/material/IconButton'
-import TextField from '@mui/material/TextField'
 import { AddressesService } from 'api/services/addresses/addresses.service'
 import Card1 from 'components/Card1'
 import { FlexBetween, FlexBox } from 'components/flex-box'
-import LazyImage from 'components/LazyImage'
 import { H6, Paragraph } from 'components/Typography'
-import { format } from 'date-fns'
-import { Formik } from 'formik'
 import { useTypedSelector } from 'hooks/useTypedSelector'
+import { method } from 'lodash'
 import { useRouter } from 'next/router'
-import { FC, useEffect, useState } from 'react'
+import { FC, useState } from 'react'
 import { useMutation, useQuery } from 'react-query'
 import { toast } from 'react-toastify'
 import { IAddress } from 'shared/types/user.types'
-import * as yup from 'yup'
 import EditAddressForm from './EditAddressForm'
 import NewAddressForm from './NewAddressForm'
 
@@ -55,16 +51,23 @@ const Heading: FC<HeadingProps> = ({ number, title }) => {
 }
 
 const CheckoutForm2: FC = () => {
+	// states
+	const [selectedAddress, setSelectedAddress] = useState<string>('')
+	const [paymentMethod, setPaymentMethod] = useState('')
+
+	// hooks
+	const router = useRouter()
+
 	// redux
 	const { user } = useTypedSelector((state) => state.userStore)
 
-	// fetching
+	// address fetching
 	const { data: addresses, refetch } = useQuery(
 		'address get',
 		AddressesService.getList
 	)
 
-	// mutation
+	// address mutation
 	const { mutateAsync: createAsync } = useMutation(
 		'address create',
 		(data: IAddress) => AddressesService.create({ ...data, user: user.id }),
@@ -86,20 +89,8 @@ const CheckoutForm2: FC = () => {
 			},
 		}
 	)
-	const { mutateAsync: deleteAsync } = useMutation(
-		'address delete',
-		(id: string) => AddressesService.delete(id),
-		{
-			onSuccess: () => {
-				toast.success('address successfully deleted')
-				refetch()
-			},
-		}
-	)
 
-	const router = useRouter()
-	const [selectedAddress, setSelectedAddress] = useState<string>('')
-
+	// handlers
 	const handleFormSubmit = async (values: any) => {
 		// router.push('/payment')
 	}
@@ -108,9 +99,13 @@ const CheckoutForm2: FC = () => {
 		setSelectedAddress(id)
 	}
 
-	const deleteAddress = async (e, id: string) => {
+	const deleteAddress = async (
+		e: React.MouseEvent<HTMLElement>,
+		id: string
+	) => {
 		e.stopPropagation()
-		deleteAsync(id)
+		await AddressesService.delete(id)
+		await refetch()
 	}
 
 	return (
@@ -166,6 +161,23 @@ const CheckoutForm2: FC = () => {
 			<Card1 sx={{ mb: 3 }}>
 				<Heading number={2} title="Payment Details" />
 
+				<FormControl>
+					<RadioGroup
+						value={paymentMethod}
+						defaultValue={payment_methods[0]?.name}
+						onChange={({ target }) => setPaymentMethod(target.value)}
+					>
+						{payment_methods?.map((method) => (
+							<FormControlLabel
+								key={method.id}
+								value={method.id}
+								control={<Radio />}
+								label={method.name}
+							/>
+						))}
+					</RadioGroup>
+				</FormControl>
+
 				<Button
 					fullWidth
 					type="submit"
@@ -180,63 +192,68 @@ const CheckoutForm2: FC = () => {
 	)
 }
 
-const addressList2 = [
-	{
-		name: 'Home',
-		phone: '+17804084466',
-		street2: '435 Bristol, MA 2351',
-		street1: '375 Subidbazaar, MA 2351',
-	},
-	{
-		name: 'Office',
-		phone: '+18334271710',
-		street2: '968 Brockton, MA 2351',
-		street1: '645 Bondorbazaar, MA 2351',
-	},
-	{
-		name: 'Office 2',
-		phone: '+17754739407',
-		street2: '777 Kazi, MA 2351',
-		street1: '324 Ambarkhana, MA 2351',
-	},
+const payment_methods: { id: string; name: string }[] = [
+	{ id: 'cash_on_dilivery', name: 'cash on dilivery' },
+	{ id: 'credit_card', name: 'credit card' },
 ]
 
-const paymentMethodList = [
-	{
-		cardType: 'Amex',
-		last4Digits: '4765',
-		name: 'Jaslynn Land',
-	},
-	{
-		cardType: 'Mastercard',
-		last4Digits: '5432',
-		name: 'Jaslynn Land',
-	},
-	{
-		cardType: 'Visa',
-		last4Digits: '4543',
-		name: 'Jaslynn Land',
-	},
-]
+// const addressList2 = [
+// 	{
+// 		name: 'Home',
+// 		phone: '+17804084466',
+// 		street2: '435 Bristol, MA 2351',
+// 		street1: '375 Subidbazaar, MA 2351',
+// 	},
+// 	{
+// 		name: 'Office',
+// 		phone: '+18334271710',
+// 		street2: '968 Brockton, MA 2351',
+// 		street1: '645 Bondorbazaar, MA 2351',
+// 	},
+// 	{
+// 		name: 'Office 2',
+// 		phone: '+17754739407',
+// 		street2: '777 Kazi, MA 2351',
+// 		street1: '324 Ambarkhana, MA 2351',
+// 	},
+// ]
 
-const timeList = [
-	{ label: '9AM - 11AM', value: '9AM - 11AM' },
-	{ label: '11AM - 1PM', value: '11AM - 1PM' },
-	{ label: '3PM - 5PM', value: '3PM - 5PM' },
-	{ label: '5PM - 7PM', value: '5PM - 7PM' },
-]
+// const paymentMethodList = [
+// 	{
+// 		cardType: 'Amex',
+// 		last4Digits: '4765',
+// 		name: 'Jaslynn Land',
+// 	},
+// 	{
+// 		cardType: 'Mastercard',
+// 		last4Digits: '5432',
+// 		name: 'Jaslynn Land',
+// 	},
+// 	{
+// 		cardType: 'Visa',
+// 		last4Digits: '4543',
+// 		name: 'Jaslynn Land',
+// 	},
+// ]
 
-const checkoutSchema = yup.object().shape({
-	card: yup.string().required('required'),
-	date: yup.string().required('required'),
-	time: yup.string().required('required'),
-	address: yup.string().required('required'),
-	cardHolderName: yup.string().required('required'),
-	cardNumber: yup.number().required('required'),
-	cardMonth: yup.string().required('required'),
-	cardYear: yup.number().required('required'),
-	cardCVC: yup.number().required('required'),
-	voucher: yup.string(),
-})
+// const timeList = [
+// 	{ label: '9AM - 11AM', value: '9AM - 11AM' },
+// 	{ label: '11AM - 1PM', value: '11AM - 1PM' },
+// 	{ label: '3PM - 5PM', value: '3PM - 5PM' },
+// 	{ label: '5PM - 7PM', value: '5PM - 7PM' },
+// ]
+
+// const checkoutSchema = yup.object().shape({
+// 	card: yup.string().required('required'),
+// 	date: yup.string().required('required'),
+// 	time: yup.string().required('required'),
+// 	address: yup.string().required('required'),
+// 	cardHolderName: yup.string().required('required'),
+// 	cardNumber: yup.number().required('required'),
+// 	cardMonth: yup.string().required('required'),
+// 	cardYear: yup.number().required('required'),
+// 	cardCVC: yup.number().required('required'),
+// 	voucher: yup.string(),
+// })
 
 export default CheckoutForm2
