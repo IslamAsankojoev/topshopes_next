6136b47d8cf9f75de622c6d4ccd0e793176cf83b
John Doe <isla.asankojoev@mail.ru>
2023-02-01 11:38:15 +0600

    colored statuses

--- src/pages-sections/admin/StyledComponents.tsx
+++ src/pages-sections/admin/StyledComponents.tsx
@@ -34,8 +34,7 @@ const StyledIconButton = styled(IconButton)(({ theme }) => ({
 	'& .MuiSvgIcon-root': { fontSize: 19 },
 	':hover': { color: theme.palette.info.main },
 }))
-type StatusType = {} & IOrderStatus
-type ShuffleStatusesType = StatusType & IShopRequestStatus
+type ShuffleStatusesType = IOrderStatus | IShopRequestStatus
 
 const StatusWrapper = styled(Box)<{ status: ShuffleStatusesType }>(
 	({ theme, status }: { theme: any; status: ShuffleStatusesType }) => {
@@ -43,7 +42,58 @@ const StatusWrapper = styled(Box)<{ status: ShuffleStatusesType }>(
 		let backgroundColor = theme.palette.secondary[100]
 
 		switch (status) {
-			case '':
+			case 'payment_error':
+				color = '#fff'
+				backgroundColor = '#f44336'
+				break
+			case 'pending':
+				color = '#fff'
+				backgroundColor = '#ff9800'
+				break
+			case 'paid':
+				color = '#fff'
+				backgroundColor = '#4caf50'
+				break
+			case 'ready':
+				color = '#fff'
+				backgroundColor = '#2196f3'
+				break
+			case 'shop_decline':
+				color = '#fff'
+				backgroundColor = '#9c27b0'
+				break
+			case 'delivering':
+				color = '#333'
+				backgroundColor = '#ffeb3b'
+				break
+			case 'delivered':
+				color = '#fff'
+				backgroundColor = '#212121'
+				break
+			case 'canceled':
+				color = '#fff'
+				backgroundColor = '#9e9e9e'
+				break
+			case 'completed':
+				color = '#fff'
+				backgroundColor = '#388e3c'
+				break
+			case 'moderation':
+				color = '#fff'
+				backgroundColor = '#9e9e9e'
+				break
+			case 'approved':
+				color = '#fff'
+				backgroundColor = '#388e3c'
+				break
+			case 'rejected':
+				color = '#fff'
+				backgroundColor = '#f44336'
+				break
+			default:
+				color = '#333'
+				backgroundColor = '#F6F8FA'
+				break
 		}
 
 		return {
