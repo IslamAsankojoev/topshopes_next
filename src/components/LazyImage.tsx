eeb5a92be7f504154528339068e905a074f5fbad
John Doe <isla.asankojoev@mail.ru>
2023-02-14 13:38:49 +0600

    images http: to https:

--- src/components/LazyImage.tsx
+++ src/components/LazyImage.tsx
@@ -13,20 +13,24 @@ import React, { useEffect } from 'react'
 const checkDomen = (url: string) => {
 	if (url?.startsWith('blob')) return url
 	if (url?.startsWith('/assets')) return url
-	return url?.startsWith('http' || 'https')
+	if (url?.startsWith('http')) return url.replace('http:', 'https:')
+	return url?.startsWith('https')
 		? url
 		: `${process.env.SERVER_URL}/media/${url}`
 }
 
 const LazyImage = styled<React.FC<ImageProps & BordersProps & SpacingProps>>(
-	({ loader, borderRadius, src, ...rest }) => (
-		<NextImage
-			objectFit="contain"
-			src={checkDomen(src as string)}
-			loader={() => checkDomen(src as string)}
-			{...rest}
-		/>
-	)
+	({ loader, borderRadius, src, ...rest }) => {
+		console.log(src)
+		return (
+			<NextImage
+				objectFit="contain"
+				src={checkDomen(src as string)}
+				loader={() => checkDomen(src as string)}
+				{...rest}
+			/>
+		)
+	}
 )(compose(spacing, borderRadius, bgcolor))
 
 export default LazyImage
